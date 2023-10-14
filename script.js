c5951df3cfc67a8f901e6286c92b9ffae1d308a3
vivian <108444857+vivbyte@users.noreply.github.com>
2023-10-14 22:33:49 +0000

    fix

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-// script.js
 function changeImage() {
     const clickableImage = document.getElementById("clickableImage");
     const overlay = document.getElementById("overlay");
     const sound = document.getElementById("sound");
 
     // Change the image
-    clickableImage.src = "assets/open.webp";
+    clickableImage.src = "assets/open.gif";
 
     // Display the overlay and play the sound
     overlay.style.opacity = 1;
+    overlay.style.display = "block"; // Ensure the overlay is visible
     sound.play();
 
     // After a delay, redirect the user
     setTimeout(function () {
-        window.location.href = "new-page.html";
-    }, 5000); // Redirect after 5 seconds (adjust the delay as needed)
+        window.location.href = "linked.html";
+    }, 8000); // Redirect after 5 seconds (adjust the delay as needed)
 }
